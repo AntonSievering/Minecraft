099db15b404adda82ec67286711d63fbdea720f7
Anton Sievering <anton@sievering.de>
2021-07-14 12:49:49 +0200

    added Collision Routines

---
+++ Minecraft/BlockId.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class BlockId : uint16_t
+{
+	AIR = 0,
+	STONE,
+	Count
+};
---
+++ Minecraft/CollisionSystem.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Hitbox.h"
+
+namespace CollisionSystem
+{
+	static inline void ResolveCollision(aabb::Hitbox3d &dbox, const aabb::Hitbox3d &sbox) noexcept
+	{
+		Engine::vf3d vOverlap    = sbox.getOverlap(dbox);
+		Engine::vf3d vAbsOverlap = std::abs(vOverlap);
+
+		if (vAbsOverlap.x < vAbsOverlap.y && vAbsOverlap.x < vAbsOverlap.z)
+		{
+			dbox.pos.x += vOverlap.x;
+		}
+		else if (vAbsOverlap.y < vAbsOverlap.x && vAbsOverlap.y < vAbsOverlap.z)
+		{
+			dbox.pos.y += vOverlap.y;
+		}
+		else
+		{
+			dbox.pos.z += vOverlap.z;
+		}
+	}
+}
